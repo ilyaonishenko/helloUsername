ff911c98046a9d227e3c3745bc644b824dbf1729
wag1 <barybin.anton@gmail.com>
2015-12-19 17:02:22 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello, Ilya");
+        System.out.println("bye, Ilya");
     }
 }
