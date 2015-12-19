2c1d1c80813acf089affa3b8e94270bacad029ff
wopqw <wopqw@yandex.ru>
2015-12-19 16:49:22 +0300

    enter username from console

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static String username;
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         System.out.println("Hello, Ilya");
+        username = getUsername();
+        System.out.println("Hello, "+username);
+    }
+    private static String getUsername() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
     }
 }
